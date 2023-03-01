817674a9ba382864981b677e84dce4769a7cf774
abudgetbox <110078091+abudgetbox@users.noreply.github.com>
2023-03-01 15:39:40 -0500

    Create Test13.java

---
+++ Test13.java
@@ -0,0 +1,28 @@
+import java.util.regex.Pattern;
+import java.util.*;
+  
+class Test10
+{
+    public static boolean isValid(String num)
+    {
+        String regExp = "^(a{2})*b((a{2})*b)*[cd]*$";
+                              
+        Pattern pattern = Pattern.compile(regExp);
+        if (num == null)
+            return false;
+        return pattern.matcher(num).matches();
+    }
+    public static void main(String[] args)
+    {
+        ArrayList<String> literal = new ArrayList<>();
+            
+          literal.add("aabbbd");
+            
+        for(String i : literal){
+            if (isValid(i))
+                System.out.println("This matches");
+            else
+                System.out.println("This does not match");
+        }
+    }
+}
