64ed6d32099cb2baec1e846b096e53631ae6d4e8
abudgetbox <110078091+abudgetbox@users.noreply.github.com>
2023-03-01 15:58:49 -0500

    Update Test15.java

--- Test15.java
+++ Test15.java
@@ -1,1 +1,28 @@
-
+import java.util.regex.Pattern;
+import java.util.*;
+  
+class Test15
+{
+    public static boolean isValid(String num)
+    {
+        String regExp = "/\\*.*\\*/";
+                              
+        Pattern pattern = Pattern.compile(regExp);
+        if (num == null)
+            return false;
+        return pattern.matcher(num).matches();
+    }
+    public static void main(String[] args)
+    {
+        ArrayList<String> literal = new ArrayList<>();
+            
+          literal.add("fdsf");
+            
+        for(String i : literal){
+            if (isValid(i))
+                System.out.println("This is a multilined comment");
+            else
+                System.out.println("This is not a multilined comment);
+        }
+    }
+}
